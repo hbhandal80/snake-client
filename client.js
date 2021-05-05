1004be804b939aec4a86a12e114fc146ce7e9cd3
Hervinder <hbhandal80@hotmail.com>
2021-05-05 21:00:48 +0000

    name added

--- client.js
+++ client.js
@@ -4,14 +4,20 @@ const net = require("net");
 const connect = function () {
   const conn = net.createConnection({
     host: "135.23.222.131",
-    port: 50542,
-    
+    port: 50542, 
+  });
+
+  // verify once connected to server
+  conn.on('connect', (data) => {
+    console.log("Successfully connected to game server");
+    conn.write('Name: HKB', () => {
+    });
   });
 
   // message if idle
   conn.on('data', (data) => {
     console.log("You ded cuz you idled");
-  })
+  });
 
   // interpret incoming data as text
   conn.setEncoding("utf8");
